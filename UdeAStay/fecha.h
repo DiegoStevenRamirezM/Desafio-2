dd493d38dc7d71b8ee0b18206d842bdd57a1e1c8
DiegoStevenRamirezM <diegosteven.ramirez@udea.edu.co>
2025-05-20 00:54:28 -0500

    Declaracion de la clase Fecha con sus atributos, constructores y métodos públicos

--- UdeAStay/fecha.h
+++ UdeAStay/fecha.h
@@ -1,10 +1,42 @@
 #ifndef FECHA_H
 #define FECHA_H
 
-class Fecha
-{
+#include <string>
+
+class Fecha {
+private:
+    int dia;
+    int mes;
+    int anio;
+
 public:
+    // Constructores
     Fecha();
+    Fecha(int d, int m, int a);
+    Fecha(const Fecha& otra);
+
+    // Destructor
+    ~Fecha();
+
+    // Getters y setters
+    int getDia() const;
+    int getMes() const;
+    int getAnio() const;
+    void setDia(int d);
+    void setMes(int m);
+    void setAnio(int a);
+
+    // Métodos
+    bool esValida() const; // Valida que la fecha sea valida
+    Fecha sumarDias(int n) const; // Suma n días (suma la cantidad de noches)
+    std::string toString() const; // Lo pasa al Formato "nombreDia, día de nombreMes del año"
+
+    //  Sobrecarga de operadores que serviran para las comparaciones entre fechas
+    bool operator==(const Fecha& otra) const;
+    bool operator<(const Fecha& otra) const;
+    bool operator>(const Fecha& otra) const;
+    bool operator>=(const Fecha& otra) const; // Añadido
+    Fecha operator+(int dias) const;
 };
 
-#endif // FECHA_H
+#endif
